6e5e93c587926f860108b19852a0074fa1f65c1b
Eliane Marion <pf1551@fiap.com.br>
2023-11-14 21:56:08 -0300

    Junção de dois vetores

--- src/Aula5/JuncaoVetor.java
+++ src/Aula5/JuncaoVetor.java
@@ -11,6 +11,33 @@ public class JuncaoVetor {
         int[] vetorA = new int[6];
         int[] vetorB = new int[6];
         int[] vetorC = new int[12];
+        int i;
+        System.out.println("==== Prenchendo o Vetor A ====");
+        for(i=0; i<6; i++){
+            System.out.println("Digite o " + (i+1) + " número: ");
+            vetorA[i] = leitor.nextInt();
+        }
+        System.out.println("==== Prenchendo o Vetor B ====");
+        for(i=0; i<6; i++){
+            System.out.println("Digite o " + (i+1) + " número: ");
+            vetorB[i] = leitor.nextInt();
+        }
+        for(i=0; i<6; i++){
+            vetorC[i] = vetorA[i]; //vetorC[1] = vetorA[1]
+            vetorC[i+6] = vetorB[i]; //vetorC[1+6] = vetorB[1]
+        }
+        System.out.println("==== Exibindo o vetor C ====");
+        for(i=0; i<12; i++)
+            System.out.println(vetorC[i]);
 
+        //Exibir todos os elementos de índice par do vetorC
+        System.out.println("==== Exibindo os elementos de índice par do vetor C ====");
+        for(i=0; i<12; i+=2){
+            System.out.print("vetorC["+ i +"]" + vetorC[i] + " ");
+        }
+        for(i=0; i<12; i++){
+            if(vetorC[i] %2 == 0)
+                System.out.print("vetorC["+ i +"] = " + vetorC[i] + " ");
+        }
     }
 }
