1db4b2a33832e98ee5212d3f772e33c0643f9845
Eliane Marion <pf1551@fiap.com.br>
2023-11-14 21:18:59 -0300

    vetor

--- src/Aula5/JuncaoVetor.java
+++ src/Aula5/JuncaoVetor.java
@@ -1,2 +1,16 @@
-package Aula5;public class JuncaoVetor {
+package Aula5;
+
+import java.util.Scanner;
+
+public class JuncaoVetor {
+    public static void main(String[] args) {
+        /*Efetuar a leitura de dois vetores com 6 posições cada.
+        * Criar um 3º vetor que será a junção dos dois anteriores*/
+
+        Scanner leitor = new Scanner(System.in);
+        int[] vetorA = new int[6];
+        int[] vetorB = new int[6];
+        int[] vetorC = new int[12];
+
+    }
 }
--- src/Aula5/Vetor.java
+++ src/Aula5/Vetor.java
@@ -1,2 +1,40 @@
-package Aula5;public class Vetor {
+package Aula5;
+
+import java.util.Scanner;
+
+public class Vetor {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double media = 0, soma = 0;
+
+        System.out.println("Por favor informe a quantidade de alunos: ");
+        int qtdAlunos = leitor.nextInt();
+
+        double[] vetorNota = new double[qtdAlunos];
+        String[] nomeAlunos = {"Ana","Guilheme", "Thiago"};
+        vetorNota[0] = 5.5;
+        vetorNota[2] = 7.0;
+
+        for(int i = 0; i < vetorNota.length; i++)
+        {
+            System.out.println("Digite a nota do " + (i+1) +"º aluno: ");
+            vetorNota[i] = leitor.nextDouble();
+        }
+        //soma = vetorNota[0]+ vetorNota[1]+vetorNota[2];
+        for(int i = 0; i < vetorNota.length; i++){
+            soma += vetorNota[i];
+        }
+        System.out.println("Soma das notas: " + soma);
+        media = soma / vetorNota.length;
+
+        for (int i = 0; i < vetorNota.length; i++ )
+            System.out.println("Nota do aluno " + (i+1) + ": " + vetorNota[i]);
+        System.out.println("\nA média da sala é: " + media);
+
+
+        for (int i = 0; i < vetorNota.length; i++ )
+            System.out.println("Nota do(a) " + nomeAlunos[i] + ": " + vetorNota[i]);
+    }
+
+
 }
